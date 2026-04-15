a79435043ece67abee96c9490c68c69489a957a0
RishitaSharma06 <rishita133pat@gmail.com>
2026-04-16 01:31:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,28 @@
-const dns=require('node:dns');
+const dns = require('node:dns');
 dns.setServers(['8.8.8.8', '8.8.4.4']);
 
-
+const mongoose = require('mongoose');
 const express = require("express");
 const cors = require("cors");
+const path = require("path"); 
 const userRoutes = require("./routes/UserRoutes.js");
-const db = require('./db.js');
+require('dotenv').config(); 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-const dbUrl=process.env.ATLASDB_URL;
-mongoose.connect(dbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log("Connected to MongoDB"))
-.catch((error) => console.error("Error connecting to MongoDB:", error));
-
-app.get('/', (req, res) => {
-  res.send('Server is running successfully 🚀');
-});
+const dbUrl = process.env.ATLASDB_URL;
+mongoose.connect(dbUrl)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((error) => console.error("Error connecting to MongoDB:", error));
 
 app.use("/api/user", userRoutes);
 
-app.listen(5000, () => {
-  console.log("server started on port 5000");
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
